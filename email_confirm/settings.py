900ece96769a768a93589faf2d6475a3dbabea79
Jordane9999 <kpadejordane@gmail.com>
2023-08-14 20:42:10 +0100

    update

--- email_confirm/settings.py
+++ email_confirm/settings.py
@@ -125,6 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Email user confirm configurations
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST = config('EMAIL_HOST')
